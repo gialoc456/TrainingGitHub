b5f1aa4a3d9f3fcc0cf4755df1b07cce920fc188
TOANNBSE62396\ADMIN <nguyenbachtoan120997@gmail.com>
2018-03-20 10:16:17 +0700

    Toan ne hihi

--- ProjectGroup/Controllers/HomeController.cs
+++ ProjectGroup/Controllers/HomeController.cs
@@ -10,13 +10,14 @@ namespace ProjectGroup.Controllers
     {
         public ActionResult Index()
         {
+            
             return View();
         }
 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
+            
             return View();
         }
 
